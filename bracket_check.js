d1dccacecf6b2bd157cc750965a2d00efbe28f6c
Edurd <yemelyanov.eduard@gmail.com>
2020-11-21 16:36:58 +0200

    //

--- bracket_check.js
+++ bracket_check.js
@@ -61,3 +61,39 @@
     }
     if (m<=0) nuberArea1.innerHTML+= ', числа не повторяются.';
   }
+
+  function test2 (){
+    let newString = prompt('введите строку на проверку','var list = function (e) {var self = this;var first, last, head;self.insert = function (value) {var node = new Node(value);if (first == null) {first = last = node;} else {var head = first;while (head.next != null) {head = head.next;}head.next = node;last = head.next;}}]');
+    let a=b=c=0;
+    let arr = [0, 0, 0];
+    for( let i=0; i<newString.length ; i++){
+      if (newString[i]=='(') arr[0]++;
+      if (newString[i]=='{') arr[1]++;
+      if (newString[i]=='[') arr[2]++;
+      if (newString[i]==')') arr[0]--;
+      if (newString[i]=='}') arr[1]--;
+      if (newString[i]==']') arr[2]--;
+    }
+    if (arr[0]==0 && arr[1]==0 && arr[2]==0) {
+      nuberArea0.innerHTML='Строка: "' + newString +'" прошла проверку.';
+      return true;
+    }
+    else {
+      for (let m = 0; m < arr.length; m++) {
+        if  (arr[m]<0) arr[m]= 'не хватает ' + arr[m] +' открывающихся скобок.';
+        if  (arr[m]>0) arr[m]= 'не хватает ' + arr[m] +' закрывающихся скобок.';
+        if (arr[m] == 0) arr[m]= 'соответствуют.';
+      }
+
+      if (arr[2]>0) {
+        a=newString.lastIndexOf('[');
+        a='<span>'+newString.slice(0, a)+'<span style="color:red;">[</span>'+newString.slice(a+1, newString.length);
+      }
+      else {
+        a=newString.lastIndexOf(']');
+        a='<span>'+newString.slice(0, a)+'<span style="color:red;">]</span>'+newString.slice(a+1, newString.length)
+      }
+      nuberArea0.innerHTML='В строке: "' + a + '"' + '<br>' +  '  скобки () '+ arr[0] +'<br>'+ ' скобки {} '+ arr[1] + '<br>'+ ' скобки [] '+ arr[2];
+      return false;
+    }
+  }
