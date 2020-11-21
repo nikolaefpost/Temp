1a70e640b5c8f8fb359c87897d01ba8cde8036d6
Edurd <yemelyanov.eduard@gmail.com>
2020-11-21 14:14:13 +0200

    //

--- bracket_check.js
+++ bracket_check.js
@@ -15,7 +15,7 @@
   }
 
   function test1 (){
-    let newString = prompt('введите строку на проверку','var list = function (e) {var self = this;var first, last, head;self.insert = function (value) {var node = new Node(value);if (first == null) {first = last = node;} else {var head = first;while (head.next != null) {head = head.next;}head.next = node;last = head.next;}}]')
+    let newString = prompt('введите строку на проверку','var list = function (e) {var self = this;var first, last, head;self.insert = function (value) {var node = new Node(value);if (first == null) {first = last = node;} else {var head = first;while (head.next != null) {head = head.next;}head.next = node;last = head.next;}}]');
     let arr = [0, 0, 0];
     for( let i=0; i<newString.length ; i++){
       if (newString[i]=='(') arr[0]++;
@@ -35,12 +35,7 @@
         if  (arr[m]>0) arr[m]= 'не хватает ' + arr[m] +' закрывающихся скобок.';
         if (arr[m] == 0) arr[m]= 'соответствуют.';
       }
-nuberArea0.innerHTML='В строке: "' + newString + '"' + '<br>' +  '  скобки () '+ arr[0] +'<br>'+ ' скобки {} '+ arr[1] + '<br>'+ ' скобки [] '+ arr[2]
-
-
-
+      nuberArea0.innerHTML='В строке: "' + newString + '"' + '<br>' +  '  скобки () '+ arr[0] +'<br>'+ ' скобки {} '+ arr[1] + '<br>'+ ' скобки [] '+ arr[2];
+      return false;
     }
   }
-//  if (k>0) nuberArea0.innerHTML='В строке: "' + newString +'" не хватает ' + k + ' закрывающихся скобок.';
-//  if (k<0) nuberArea0.innerHTML='В строке: "' + newString +'" не хватает ' + k + ' открывающихся скобок.';
-//  else nuberArea0.innerHTML='Строка: "' + newString +'" прошла проверку.';
